93bce407fd92693636126758b9c670d232be750c
9s2mil <124743931+9s2mil@users.noreply.github.com>
2025-08-22 12:42:16 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,33 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("pwa-clock").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
+const CACHE_NAME = "clock-cache-v20250821";
+
+const ASSETS_TO_CACHE = [
+  "index.html",
+  "manifest.json",
+  "service-worker.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
